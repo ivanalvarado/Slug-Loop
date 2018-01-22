b2cabe547feb691f64f158ff1eb795ff45058461
Ivan Alvarado <ivalvara21@gmail.com>
2018-01-22 11:12:10 -0800

    Started single view application, no code yet

---
+++ SlugBus/SlugBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlugBus
+//
+//  Created by Ivan Alvarado on 1/22/18.
+//  Copyright © 2018 UCSC Slugs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
