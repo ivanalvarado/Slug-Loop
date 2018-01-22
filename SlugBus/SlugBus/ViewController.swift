1ecbe087452d44ce1bedd0a287f21e3da46a7c02
Ivan Alvarado <ivalvara21@gmail.com>
2018-01-22 11:53:54 -0800

    centered MKMapView on UCSC campus

--- SlugBus/SlugBus/ViewController.swift
+++ SlugBus/SlugBus/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Center map on UCSC campus
+        let ucscCampusLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(36.9900, -122.0605)
+        let distanceSpan: MKCoordinateSpan = MKCoordinateSpanMake(0.03, 0.03)
+        mapView.setRegion(MKCoordinateRegionMake(ucscCampusLocation, distanceSpan), animated: true)
     }
 
     override func didReceiveMemoryWarning() {
