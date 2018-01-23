8936f4375272c9714f70d974474c7b610c66bc1b
Ivan Alvarado <ivalvara21@gmail.com>
2018-01-22 16:46:29 -0800

    Prompting user for access to their current location and displaying it on the map if granted

--- SlugBus/SlugBus/ViewController.swift
+++ SlugBus/SlugBus/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +23,46 @@ class ViewController: UIViewController {
         let ucscCampusLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(36.9900, -122.0605)
         let distanceSpan: MKCoordinateSpan = MKCoordinateSpanMake(0.03, 0.03)
         mapView.setRegion(MKCoordinateRegionMake(ucscCampusLocation, distanceSpan), animated: true)
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // If we don't have access to the user's current location, request it
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    /*
+     Called whenever the user's location is updated.
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .denied:
+            print("Denied")
+            break
+        case .notDetermined:
+            print("Not Determined")
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .authorizedWhenInUse:
+            print("Authorized When In Use")
+            mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
+            break
+        default:
+            break
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
